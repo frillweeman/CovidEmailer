09fa6c92be67b4723e5823a41f85e83d14e6c4f3
Will Freeman <hohosanta@me.com>
2020-03-29 17:55:08 -0500

    fix service account

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,15 +1,14 @@
-const config = require("firebase-functions").config();
 const test = require("firebase-functions-test")(
   {
     databaseURL: "https://alavirus-61e7d.firebaseio.com",
     projectId: "alavirus-61e7d",
     storageBucket: "alavirus-61e7d.appspot.com"
   },
-  "alavirus-61e7d-firebase-adminsdk-qrhzr-8314323fea.json"
+  "alavirus-firebase-key.json"
 );
 
 // use real data for test data
-test.mockConfig(config);
+// test.mockConfig(config);
 
 const functions = require("../index.js");
 
